f63f65d027c07cef431bdfd96a6442b00d4b0017
nesro <nesro@nesro.cz>
2016-11-20 22:21:47 +0100

    manage list of shellcheck exceptions. make it easier to add them

--- hooks/cshook_shellcheck.sh
+++ hooks/cshook_shellcheck.sh
@@ -4,7 +4,31 @@
 sc_check=1 # check every command with ShellCheck before executing
 sc_path=~/.cabal/bin/shellcheck
 sc_debug=0
-#sc_args="disable=SC2164"
+
+cssc_disable_list_file="/home/n/.commash/hooks/cshook_shellcheck_disable_list.txt"
+cssc_disable=""
+
+cssc_load_disable_list() {
+	if [[ ! -f $cssc_disable_list_file ]]; then
+		>&2 echo ",: ShellCheck: disable list file \"$cssc_disable_list_file\" not found"
+		return
+	fi
+
+	cssc_disable=""
+	while IFS='' read -r line || [[ -n "$line" ]]; do
+		line=$(echo "$line" | awk -F'#' '{ print $1 }' | tr -d ' ')
+		if [[ -z "$line" ]]; then
+			continue
+		fi
+		if [[ -z "$cssc_disable" ]]; then
+			cssc_disable="$line"
+		else
+			cssc_disable="$cssc_disable,$line"
+		fi
+	done < "$cssc_disable_list_file"
+
+	echo -n "$cssc_disable"
+}
 
 # TODO: call this hook on commash install
 cshook_shellcheck_install() {
@@ -18,8 +42,11 @@ cshook_shellcheck_before() {
 	# variables and the actual command
 	#sc_out=$($sc_path <(echo '#!/bin/bash'; echo "$(set -o posix ; set)"; echo "$cmd") 2>&1)
 	
-	#TODO: create an external list of things to disable
-	sc_out=$($sc_path <(echo -e "#!/bin/bash\n# shellcheck disable=2043,2164,2154,2034\n$cmd") 2>&1)
+	if [[ -z "$cssc_disable" ]]; then
+		cssc_disable=$(cssc_load_disable_list)
+	fi
+
+	sc_out=$($sc_path <(echo -e "#!/bin/bash\n# shellcheck disable=$cssc_disable\n$cmd") 2>&1)
 	sc_rc=$?
 	sc_out=$(echo "$sc_out" | tail -n +3)
 	
@@ -28,7 +55,7 @@ cshook_shellcheck_before() {
 	if (( sc_rc > 0 )); then
 		echo -e ",: ShellCheck: \n$sc_out"
 		
-		echo -n ",: Now what? [r]un, [s]top: "
+		echo -n ",: Now what? [r]un, [s]top, [i]gnore: "
 		while :; do
 			read -rn1 key
 				
@@ -39,6 +66,27 @@ cshook_shellcheck_before() {
 				
 			if [[ $key == s ]]; then
 				echo -e "\n,: Stopping the command."
+				return 1
+			fi
+
+			if [[ $key == i ]]; then
+				cat <<-EOF
+
+
+,: Ignoring warnings from ShellCheck is easy, but not automatic.
+,: Please look into the detail of the check. If you want to ignore this check
+,: in the future, add the number to the disable list file. You can use the
+,: prepared command.
+,: (XXX: The changes takes place in the new session.)
+
+				EOF
+
+				for warn in $(echo "$sc_out" | grep -o 'SC[0-9]*' | tr -d SC ); do
+					echo "https://github.com/koalaman/shellcheck/wiki/SC$warn"
+					echo "echo $warn >> $cssc_disable_list_file"
+					echo
+				done
+
 				return 1
 			fi
 				
