4bd8b9fed285f5794899c0eca480fd6b8d64ce8b
nesro <nesro@nesro.cz>
2016-12-07 16:45:55 +0100

    remove trailing whitespace

--- comma.sh
+++ comma.sh
@@ -25,7 +25,7 @@ if [[ -z $COMMASH_REDIRECT ]]; then
 	# the biggest problem atm is that program like 'less' and 'more' doesn't
 	# scroll as supposed
 	# there is also a problem with buffering.
-	
+
 	# the best solution is to write our version of the "script" program
 	# or just use the standard version and write our timestamps to the output
 	# file
@@ -50,19 +50,19 @@ cat <<EOF
 TODO:
 	- add arguments to ShellCheck (f.ex: disable=SC2164 for enabling cd without
 		|| exit)
-		
+
 	- save commands into a history file. store pid of shell, return code of
 		the executed command, timestamp
-		
+
 	- add ,info command that tells informations about a file
-	
+
 	- add ,cd command that allows quickly cd directory (from actual dir or
 		backwards in current path)
-		
+
 	- save stdout and stderr for every command executed
-	
+
 	- use /tmp to store .commash/lock (/tmp is tmpfs and should be faster)
-	
+
 	- add more layers of safe mode
 		- layer 1) don't make any changes, just inform what's happening
 		- layer 2) make changes, but backup everything and allow easy
@@ -73,60 +73,60 @@ TODO:
 				that would eat up a lot of disk space
 			- the ultimate goal is to provide layer 2 without user interaction
 				and bothering him while still providing usefull things
-	
+
 	- don't run absolute paths in safe mode (then it wouldn't be safe)
-	
+
 	- add more commands into safe mode (chmod, rm, mv, cp, ln, ...)
-	
+
 	- make some kind of persistent storage of internal variables
 		(f.ex.: cs_AUTOENABLE)
-	
+
 	- make some fancy PS1 as an option (time, hostname, path, rc, shlvl)
-	
+
 	- add ,d <command> (debugging of a particular command)
-	
+
 	- inspect commands when rc != 0 and write human-readable info what happened
 		(for example "man asdf" returns 16, even if the man write what happend
 		 it would be nice if we could do it too)
-	
+
 	- if we want to exit the shell now, the bind is: ctrl-x because ctrl-d
-	
+
 	- the current code rewrites previous PROMPT_COMMAND (which is wrong)
-	
+
 	- refactor function names and variables to be consistent
-	
+
 	- DONE detect things like:
 		- $ cd /a
 		- $ cd /b
 		- $ cd /a
 		- and then suggest: cd -
-	
+
 	- refactor debugger (to be more modular) and add more functionality
-	
+
 	- add support for coloring output: white for stdout, red for stderr
 		(I can do it, but I cannot change directory while doing that)
-		
+
 	- is the lock file really necessary? maybe a variable would be sufficient
 		right now, this commands works fine:
 		false || true && ((echo a; (echo b; echo c); echo $(echo d)) && echo "$(echo $(echo $(echo e)))"; echo f) && echo g
-		
+
 	- fix: bash: return: can only `return' from a function or sourced script
-	
+
 	- support more languages (english and czech)
-	
+
 	- todo: make an option to suppress/disable hooks/tips
-	
+
 	- add time in UTC. saving history while operating on servers around the
 		world would be easier
-	
+
 	- run safe/unsafe versions of commands by prepending , before them:
 		,rm <file>
-		
+
 	- safe version of rm should just move the file to .$origname.csrm
-	
+
 	- add a post-hook that inform you about the command end of execution.
 		via email, sound, change of title/screen title
-	
+
 	- save all terminal history with timestamps and return codes?
 		https://stackoverflow.com/questions/3173131/redirect-copy-of-stdout-to-log-file-from-within-bash-script-itself
 EOF
@@ -142,7 +142,7 @@ else
 fi
 
 #-------------------------------------------------------------------------------
-# These variables 
+# These variables
 
 
 # auto enable commash with .bashrc
@@ -173,7 +173,7 @@ Usage:
 	,safe
 	,s
 		turn on safe mode
-		
+
 	,nsafe
 	,ns
 		turn off safe mode
@@ -184,7 +184,7 @@ Usage:
 	,usage
 	,u
 		show this usage
-	
+
 	,exit
 	,x
 	ctrl + x
@@ -295,7 +295,7 @@ cs_SAFE=0
 csfunc_safe() {
 	csfunc_inside=1
 	cs_SAFE=$1
-	
+
 	if [[ $1 == 1 ]]; then
 		PATH="$cs_SAFEDIR:$PATH"
 		echo ",: safe mode enabled. PATH is \"$PATH\""
@@ -329,9 +329,9 @@ cs_disable() {
 
 cs() {
 	[[ $cs_INTERNAL_DEBUG =~ comma|all ]] && set -xv
-	
+
 	local arg="$1"
-	
+
 	case $arg in
 	e|enable)
 		cs_enable "$@"
@@ -373,7 +373,7 @@ csfunc_debug() {
 
 	cs_DEBUG="$1"
 	echo ",: commash debug has been set to: \"$1\""
-	
+
 	csfunc_inside=0
 }
 alias ,debug="csfunc_debug 1"
@@ -388,51 +388,51 @@ alias ,n=",nd"
 
 csfunc_inside_debugger() {
 	cmd=$(HISTTIMEFORMAT='' history 1 | sed -e "s/^[ ]*[0-9]*[ ]*//")
-	
+
 	echo ",dbg cmd: \"$cmd\""
-	
-	
-	
-	
+
+
+
+
 	echo -n ",: Now what? show [a]st, e[x]it debugger, execute some [p]ipelines, do [n]othing: "
 	while :; do
 		read -rn1 key
-		
+
 		if [[ $key == p ]]; then
-			
+
 			echo "\n,dbg: Showing simple commands saved from bash:"
 			if (( cspc_command_cnt > 0 )); then
 				for (( cspc_i=0 ; cspc_i < cspc_command_cnt ; cspc_i++ )); do
 					echo "$cspc_i=\"${cs_command_arr[$cspc_i]}\""
 				done
 			fi
-		
+
 			echo -n ",dbg: How many pipelines you want to run: "
 			read -r pipelines
-			
+
 			pipcmd="${cs_command_arr[0]}"
 			for (( i=1 ; i < pipelines ; i++ )); do
 				pipcmd="$pipcmd | ${cs_command_arr[i]}"
 			done
-			
+
 			echo -e "\n,dbg: executing: \"$pipcmd\""
 			eval "$pipcmd"
-			
+
 			# TODO: catch rc
 			break
 		fi
-		
+
 		if [[ $key == x ]]; then
 			echo -e "\n,: Exit from the debugger."
 			cs_DEBUGGER=0
 			break
 		fi
-		
+
 		if [[ $key == n ]]; then
 			echo -e "\n,: Do nothing. You're still in debugger."
 			break
 		fi
-		
+
 		if [[ $key == a ]]; then
 			echo -e "\n,: Showing ast (output from python script):"
 			~/.commash/ast.py "$cmd"
@@ -451,7 +451,7 @@ cspc_command_cnt=0
 
 csfunc_locked() {
 	if [[ -f ~/.commash/lock ]]; then
-		return 0 # true, not locked 
+		return 0 # true, not locked
 	else
 		return 1
 	fi
@@ -482,14 +482,14 @@ csfunc_prompt() {
 
 	if [[ $cs_AUTOENABLE == 1 ]] && [[ $cspc_first == 1 ]]; then
 		cspc_first=0
-		
+
 		echo ",: Autoenabling commash in the first PROMPT_COMMAND"
-		
+
 		cs_ENABLED=1
 	fi
 
 	csfunc_unlock
-	
+
 
 	if [[ $cs_DEBUGGER == 1 ]]; then
 		csfunc_inside_debugger
@@ -502,8 +502,8 @@ csfunc_prompt() {
 	# The debug trap is called more than once per command.
 	# This must be the last command
     csfunc_catch_command=1
-    
-    
+
+
     csfunc_inside=0
 }
 #-------------------------------------------------------------------------------
@@ -551,13 +551,13 @@ csfunc_hook_add_after() {
 # <hook> <command timestamp> <command>
 csfunc_hook_iterate_before() {
 	local ret=0
-	
+
 	for i in "${!cs_HOOKS_BEFORE[@]}"; do
 		if ! ${cs_HOOKS_BEFORE[$i]} "$1" "$2"; then
 			ret=1
 		fi
 	done
-	
+
 	return $ret
 }
 csfunc_hook_iterate_after() {
@@ -585,9 +585,9 @@ cs_add_timestamp() {
 
 csfunc_debug_trap() {
 	cs_debug_trap_rc=$?
-	
+
 	#---------------------------------------------------------------------------
-	
+
 	# If we control-c a command, we jump again into the debug trap
 	# this is the right place to explain what happened.
 	if (( cs_debug_trap_rc == 130 )) && (( cs_debug_trap_rc_ctrlc == 0 )); then
@@ -599,8 +599,8 @@ csfunc_debug_trap() {
 		csfunc_inside=0
 		return 1
 	fi
-	
-	
+
+
 	#---------------------------------------------------------------------------
 	# These are cases where we execute the command.
 
@@ -620,45 +620,45 @@ csfunc_debug_trap() {
 	if [[ $BASH_COMMAND =~ ^csfunc_ ]] || [[ $csfunc_inside == 1 ]]; then
 		return 0
 	fi
-	
+
 	# This is the case when you press the <tab> key and bash is trying to
 	# autocomplete.
 	if [[ -n $COMP_LINE ]]; then
 		return 0
 	fi
-	
+
 	#---------------------------------------------------------------------------
-	
+
 	# We will get error from set -u if we do not assign default values here.
 	cs_rc=${cs_rc:-0}
 	cs_last=${cs_last:-''}
 	COMP_LINE=${COMP_LINE:-''}
-	
+
 	#---------------------------------------------------------------------------
-	
+
 	# Save BASH_COMMAND for allow debuger partial execution
 	cs_command_arr[$cspc_command_cnt]="$BASH_COMMAND"
 	(( cspc_command_cnt++ ))
-	
-	#---------------------------------------------------------------------------	
-		
+
+	#---------------------------------------------------------------------------
+
 
 	if (( csfunc_catch_command == 1 )) && [[ ! -f ~/.commash/lock ]]; then
 		csfunc_catch_command=0
 		csfunc_lock
-		
+
 		# If we ctrlc this command, show the warning
 		cs_debug_trap_rc_ctrlc=0
 
-		# Get last command from history without its number		
+		# Get last command from history without its number
 		cmd=$(HISTTIMEFORMAT='' history 1 | sed -e "s/^[ ]*[0-9]*[ ]*//")
-		
+
 		# if debug mode is off, run the command
 		if [[ $cs_DEBUG == 0 ]]; then
-			
+
 			# generate timestamp and use it as command identifier (for hooks)
 			cs_timestamp=$(date +%Y-%m-%d-%H-%M-%S-%N)
-			
+
 			# Any _before hook can prevent command execution
 			if csfunc_hook_iterate_before "$cs_timestamp" "$cmd"; then
 
@@ -686,30 +686,30 @@ csfunc_debug_trap() {
 	set +u
 
 				"
-		
+
 		#>(cs_add_timestamp "out") 2>(cs_add_timestamp "err" >&2)
-			
+
 				cs_rc=$(echo $cs_bash_internals | awk -F "CSDELIMETER" '{ print $2 }')
 				cs_last=$(echo $cs_bash_internals | awk -F "CSDELIMETER" '{ print $1 }')
-			
+
 				echo "[CSLOG|$cs_timestamp|rc|$cs_rc]" >> $CSLOG
-			
+
 				# FIXME: multiline commands?
 				#echo "$cs_timestamp \"$cmd\" $cs_rc $(pwd)" >> $cs_LOGFILE
-			
+
 				csfunc_hook_iterate_after "$cs_timestamp" "$cmd"
 			fi
 		else
 			echo ",: commash prevented execution of: \"$cmd\""
 			echo ",: going to the debugger mode"
-			
+
 			cs_DEBUGGER=1
 		fi # cs_DEBUG == 0
-		
+
 	fi
-	
+
 	#---------------------------------------------------------------------------
-	
+
 	return 1
 }
 
@@ -719,8 +719,8 @@ cs_main() {
 	cs_run_install_if_needed
 	csfunc_hook_init
 	csfunc_debug_trap_enable
-	
-	
+
+
 #	cat <<EOF
 #,: Commash has been loaded. But it's disabled by default.
 #,: You can enable it by running: ", enable" (or just ", e" )
@@ -739,7 +739,7 @@ cs_main
 if false; then
 
 		# eval the commmand and color the output
-		echo -en "\033[31m"  ## red		
+		echo -en "\033[31m"  ## red
 		#eval "$cmd" | while read -r line; do
 		while read -r line; do
     		echo -en "\033[36m"  ## blue
@@ -748,8 +748,8 @@ if false; then
 		done < <(eval "$cmd")
 		cs_rc=${PIPESTATUS[0]}
 		echo -en "\033[0m"  ## reset color
-		
+
 		echo ",: mid pwd=$(pwd)"
-				
+
 fi #false
 
