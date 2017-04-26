681a37616f4e2e5e8c8c82611476d68cf14ff303
nesro <nesro@nesro.cz>
2017-04-26 17:39:17 +0200

    explain rc for man, some examples of shellcheck and ps4 to thesis

--- hooks/cshook_explain_rc.sh
+++ hooks/cshook_explain_rc.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
+# TODO: more modular explanation for particular commands
+
+csfunc_explain_rc_none() {
+	echo "(no explanation)"
+}
+
 cs_explain_rc() {
 	local rc=$1
+	local cmd="$2"
 
 	case $rc in
 	1)
@@ -11,6 +18,13 @@ cs_explain_rc() {
 		# FIXME: this is not working properly, because command with this rc
 		# is not in the history
 		echo "(Misuse of shell builtins)"
+		;;
+	16)
+		if [[ $cmd =~ ^man ]]; then
+			echo "(At least one of the pages/files/keywords didn't exist or wasn't matched.)"
+		else
+			csfunc_explain_rc_none
+		fi
 		;;
 	126)
 		echo "(Command invoked cannot execute)"
@@ -29,25 +43,29 @@ cs_explain_rc() {
 		echo "(Control-Z)"
 		;;
 	*)
-		echo "(no explanation)"
+		csfunc_explain_rc_none
 		;;
 	esac
 }
 
 csfunc_rc() {
 	local rc=$1
+	local cmd="$2"
 
 	# Don't print about command not found when there is an active hook for it
 	if (( rc == 127 )) && [[ -n "$cs_HOOK_NOTFOUND_ACTIVE" ]]; then
 		return
 	fi
 
-	echo ",: return code warning: \$? == $rc $(cs_explain_rc "$rc")"
+	echo ",: return code warning: \$? == $rc $(cs_explain_rc "$rc" "$cmd")"
 }
 
 cshook_explain_rc_after() {
+	local timestamp="$1"
+	local cmd="$2"
+
 	if (( cs_rc > 0 )); then
-		csfunc_rc "$cs_rc"
+		csfunc_rc "$cs_rc" "$cmd"
 	fi
 }
 
